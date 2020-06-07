57f76d8a9aafee33808d4b211b7a75875af5e040
progfay <shun1856shun@gmail.com>
2020-06-07 17:50:32 +0900

    chore: mv storybook files into one dir

---
+++ stories/.storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  stories: ['../**/*.stories.tsx'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [{ loader: require.resolve('ts-loader') }]
+    })
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
+  }
+}
